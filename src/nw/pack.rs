b6c1838dfb37e4e2255e100b4f81ae3343028a8b
iegad <iegad@vip.qq.com>
2022-09-20 11:49:53 +0800

    Update pack.rs

--- src/nw/pack.rs
+++ src/nw/pack.rs
@@ -1,3 +1,10 @@
+/// -----------------------------------
+/// Package 消息包
+/// author: iegad
+/// time:   2022-09-20
+/// update_timeline:
+/// | ---- time ---- | ---- editor ---- | ------------------- content -------------------
+
 use crate::g;
 use bytes::{Buf, BufMut, BytesMut};
 use lazy_static::lazy_static;
@@ -5,11 +12,13 @@ use lockfree_object_pool::LinearObjectPool;
 use std::mem::size_of;
 use type_layout::TypeLayout;
 
-/// Package 消息包, 用于分布式网络通信
+/// # Package 消息包
+/// 
+/// 用于分布式网络通信
 /// 
 /// 消息包由消息头与消息体组成
 /// 
-/// 消息头包含以下五个字段:
+/// ## 消息头包含以下五个字段:
 /// 
 /// [service_id]: 16位: 服务ID, 确定所调用的服务
 /// 
@@ -19,37 +28,50 @@ use type_layout::TypeLayout;
 /// 
 /// [idempotent]: 32位: 幂等
 /// 
-/// [raw_len]:    32位: 消息长度
+/// [raw_len]:    32位: 消息长度, 包括消息头16字节与消息体长度
 #[derive(TypeLayout, Debug)]
 #[repr(C)]
 pub struct Package {
-    service_id: u16,
-    package_id: u16,
-    router_id: u32,
-    idempotent: u32,
-    raw_len: usize,
-    data: BytesMut,
+    service_id: u16, // 服务ID
+    package_id: u16, // 包ID
+    router_id: u32,  // 路由ID
+    idempotent: u32, // 幂等
+    raw_len: usize,  // 包长
+    data: BytesMut,  // 消息体
 }
 
 lazy_static! {
+    /// PACK_POOL: 框架内部使用 package 对象池
     pub(crate) static ref PACK_POOL: LinearObjectPool<Package> =
         LinearObjectPool::new(|| Package::new(), |v| { v.reset() });
+
+    /// REQ_POOL: 请求使用 package 对象池
     pub static ref REQ_POOL: LinearObjectPool<Package> =
         LinearObjectPool::new(|| Package::new(), |v| { v.reset() });
+
+    /// RSP_POOL: 应答使用 package 对象池
     pub static ref RSP_POOL: LinearObjectPool<Package> =
         LinearObjectPool::new(|| Package::new(), |v| { v.reset() });
 }
 
 impl Package {
+    /// 消息头长度
     pub const HEAD_SIZE: usize = size_of::<u16>()
         + size_of::<u16>()
         + size_of::<u32>()
         + size_of::<u32>()
         + size_of::<u32>();
+    
+    /// 消息体最大长度: 1G
     pub const MAX_DATA_SIZE: usize = 1024 * 1024 * 1024;
+
+    /// 16位加密KEY, 用于消息头中16位字段的加密和解密
     const HEAD_16_KEY: u16 = 0x0A0B;
+
+    /// 32位加密KEY, 用于消息头中32位字段的加密和解密
     const HEAD_32_KEY: u32 = 0x0C0D0E0F;
 
+    /// 创建空包Package, 空包长度为 Package::HEAD_SIZE
     pub fn new() -> Self {
         Self {
             service_id: 0,
@@ -61,6 +83,7 @@ impl Package {
         }
     }
 
+    /// 通过入参创建 Package
     pub fn with_params(
         service_id: u16,
         package_id: u16,
@@ -78,6 +101,28 @@ impl Package {
         }
     }
 
+    /// 通过 rbuf 缓冲区构建 pack
+    #[inline(always)]
+    pub fn parse(rbuf: &mut BytesMut, pack: &mut Self) -> g::Result<()> {
+        if rbuf.len() == 0 {
+            return Err(g::Err::PackDataSizeInvalid);
+        }
+
+        if pack.head_valid() {
+            if !pack.valid() {
+                pack.fill_data(rbuf);
+            }
+        } else {
+            if rbuf.len() < Self::HEAD_SIZE {
+                return Err(g::Err::PackDataSizeInvalid);
+            }
+            pack.from_buf(rbuf)?;
+        }
+
+        Ok(())
+    }
+
+    /// 通过BytesMut 缓冲区 创建 Package
     pub fn from(rbuf: &mut BytesMut) -> g::Result<Self> {
         if rbuf.len() < Self::HEAD_SIZE {
             return Err(g::Err::PackHeadInvalid("Head Size is invalid"));
@@ -124,11 +169,7 @@ impl Package {
         })
     }
 
-    #[inline(always)]
-    pub fn reset(&mut self) {
-        self.service_id = 0;
-    }
-
+    /// 通过BytesMut 缓冲区构建当前 Package对象
     pub fn from_buf(&mut self, rbuf: &mut BytesMut) -> g::Result<()> {
         if rbuf.len() < Self::HEAD_SIZE {
             return Err(g::Err::PackHeadInvalid("Head Size is invalid"));
@@ -167,6 +208,13 @@ impl Package {
         Ok(())
     }
 
+    /// 重置 Package
+    #[inline(always)]
+    pub fn reset(&mut self) {
+        self.service_id = 0;
+    }
+
+    /// 将Package包 序列化到一个新的 BytesMut字节缓冲区
     #[inline(always)]
     pub fn to_bytes(&self) -> BytesMut {
         let mut wbuf = BytesMut::with_capacity(self.raw_len);
@@ -182,8 +230,17 @@ impl Package {
         wbuf
     }
 
+    /// 按需填充消息体, 当该包被半加载时调用.
+    /// 
+    /// 当包的消息头已构建, 但是消息体不全时调用此方法.
+    /// 
+    /// 如果 rbuf 缓冲区的内容大于 当前Package 消息体所需长度时, 消息体只会从rbuf 中消费掉需要的数据长度.
+    /// 
+    /// 该函数不同于[append_data], 该函数是在接收包时构建Package中使用.
     #[inline(always)]
     pub fn fill_data(&mut self, rbuf: &mut BytesMut) {
+        debug_assert!(self.head_valid());
+
         let rbuf_len = rbuf.len();
 
         let mut left_len = self.raw_len - Self::HEAD_SIZE - self.data.len();
@@ -195,17 +252,24 @@ impl Package {
         rbuf.advance(left_len);
     }
 
+    /// 追加数据到消息体中
+    /// 
+    /// 该函数不同于[fill_data], 该函数是在主动发包时构建Package中使用.
     #[inline(always)]
     pub fn append_data(&mut self, data: &[u8]) {
         self.data.extend_from_slice(data);
         self.raw_len += data.len();
+
+        debug_assert!(self.data.len() <= Self::MAX_DATA_SIZE);
     }
 
+    /// 消息头是否有效
     #[inline(always)]
     pub fn head_valid(&self) -> bool {
         self.service_id > 0 && self.package_id > 0 && self.router_id > 0 && self.idempotent > 0
     }
 
+    /// 包是否有效
     #[inline(always)]
     pub fn valid(&self) -> bool {
         self.raw_len == Self::HEAD_SIZE + self.data.len() && self.head_valid()
@@ -253,7 +317,7 @@ impl Package {
 
     #[inline(always)]
     pub fn set_data(&mut self, data: &[u8]) {
-        assert!(data.len() < Self::MAX_DATA_SIZE);
+        assert!(data.len() <= Self::MAX_DATA_SIZE);
 
         self.data.clear();
         self.data.extend_from_slice(data);
@@ -269,26 +333,6 @@ impl Package {
     #[inline(always)]
     pub fn raw_len(&self) -> usize {
         self.raw_len
-    }
-
-    #[inline(always)]
-    pub fn parse(rbuf: &mut BytesMut, pack: &mut Self) -> g::Result<()> {
-        if rbuf.len() == 0 {
-            return Err(g::Err::PackDataSizeInvalid);
-        }
-
-        if pack.head_valid() {
-            if !pack.valid() {
-                pack.fill_data(rbuf);
-            }
-        } else {
-            if rbuf.len() < Self::HEAD_SIZE {
-                return Err(g::Err::PackDataSizeInvalid);
-            }
-            pack.from_buf(rbuf)?;
-        }
-
-        Ok(())
     }
 }
 
