a4668df33962446d3eb2d3e33736f64f8d798188
iegad <iegad@vip.qq.com>
2022-10-24 14:59:24 +0800

    update

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
-use std::path::PathBuf;
-
 fn main() {
-    let out_dir = PathBuf::from("./src/pb");
-    if let Err(err) = tonic_build::configure().file_descriptor_set_path(out_dir).compile(&["src/proto/warsong.proto"], &[""]) {
+    if let Err(err) = tonic_build::configure().out_dir("src/proto").compile(&["warsong.proto"], &["src/proto"]) {
         panic!(">>>>>>>>>>> {err}");
     }
 }
